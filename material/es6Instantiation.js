4dd70e32fdb2735e1c960893f4e985a134ed7849
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-10-22 11:03:18 -0300

    update es6Instantiation

---
+++ material/es6Instantiation.js
@@ -0,0 +1,39 @@
+class FullName {
+	firstName;
+	lastName;
+
+	constructor(firstName, lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+}
+
+class Animal {
+	type;
+	fullName;
+	age;
+	energy = 0;
+
+	constructor(type, nomeCompleto, age) {
+    if(nomeCompleto instanceof FullName) {
+      this.type = type;
+      this.fullName = nomeCompleto;
+      this.age = age;
+    } else {
+      throw 'fullName precisa ser instância da classe FullName'
+    }
+	}
+
+	eat() {
+		//...
+	}
+
+	sleep() {
+		//...
+	}
+}
+
+const fullNameAnimal1 = new FullName('Aslam', 'Rangel');
+
+const animal1 = new Animal('cachorro', fullNameAnimal1, 3);
+console.log(animal1);
