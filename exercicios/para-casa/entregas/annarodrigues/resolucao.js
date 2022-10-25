60aeda495aa6b6da667b580acf13a349c645caf0
amrodrigues <42442977+amrodrigues@users.noreply.github.com>
2022-10-25 10:27:18 -0300

    atualizacao

--- exercicios/para-casa/entregas/annarodrigues/resolucao.js
+++ exercicios/para-casa/entregas/annarodrigues/resolucao.js
@@ -1,27 +1,26 @@
-const Bank = require ("./bank");
 const Client = require("./client");
+const Bank = require ("./bank");
 const BankAccount = require("./bankAccount");
+
 
 const bank1 = new Bank(1, "Bradesco", 0.01);
 const bank2 = new Bank(2, "Santander", 0.01);
 
-
 const cliente1 = new Client("Cláudia", 123);
-const cliente2 = new Client("ANTONIO", 123);
+const cliente2 = new Client("Antonio", 123);
 
 cliente1.addBank(bank1);
 cliente1.addBank(bank2);
 cliente2.addBank(bank1);
-cliente1.removeBank(bank1);
- 
- 
+cliente2.removeBank(bank1);
+
 const bankAccount2 = new BankAccount(cliente1, bank2, 1111, 2222); // Instanciação de um objeto BankAccount.
 bankAccount2.Credit(1000)
-bankAccount2.Debit(10)
-bankAccount2.cashWithdrawal(200)
-bankAccount2.cashWithdrawal(200)
-bankAccount2.cashWithdrawal(200)
-bankAccount2.cashWithdrawal(200)
-bankAccount2.cashWithdrawal(50)
+bankAccount2.Debit(200)
+bankAccount2.cashWithdrawal(220)
+bankAccount2.cashWithdrawal(20)
+bankAccount2.cashWithdrawal(20)
+bankAccount2.cashWithdrawal(20)
+bankAccount2.cashWithdrawal(20)
 
 bankAccount2.closeAccount();
