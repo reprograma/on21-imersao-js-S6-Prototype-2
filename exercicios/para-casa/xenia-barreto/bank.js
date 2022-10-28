96de13e52d055f483d94e573176222c889f1601d
Xenia Barreto <xeniabarreto22@gmail.com>
2022-10-28 01:19:07 -0300

    Fix: Update in file bank.js

--- exercicios/para-casa/xenia-barreto/bank.js
+++ exercicios/para-casa/xenia-barreto/bank.js
@@ -12,13 +12,4 @@ class Bank {
   static createdBanks = [];
 }
 
-
-/*Bancos criados
-
-Propriedade estática, sendo ela uma array de objetos que é inicialmente vazia e é atualizada a cada vez que um novo banco é criado, contendo:
-Código do banco criado
-Quantidade de clientes que esse banco possui
-Esse valor deve ser inicializado com 0 e aumentar a medida que um cliente é associado a esse banco.
-*/
-
 module.exports = Bank
