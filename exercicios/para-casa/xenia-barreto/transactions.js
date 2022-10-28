edc409bb32db5bbe6e715275e225fada2d1c2b9c
Xenia Barreto <xeniabarreto22@gmail.com>
2022-10-28 02:09:29 -0300

    Fix: Updates in the transaction file

--- exercicios/para-casa/xenia-barreto/transactions.js
+++ exercicios/para-casa/xenia-barreto/transactions.js
@@ -18,7 +18,7 @@ const client4 = new Client('Juliana', 54655286545)
 
 
 // console.log(Bank.createdBanks);
-client1.addBank(bankInter)
+// client1.addBank(bankInter)
 // client2.addBank(bankInter)
 // client3.addBank(bankInter)
 // console.log(Bank.createdBanks);
@@ -28,16 +28,23 @@ client1.addBank(bankInter)
 // console.log(client1);
 // console.log(Bank.createdBanks);
 
-const bankAccount1 = new BankAccount(client1, bankInter, 2950, 156826-5)
-
+// const bankAccount1 = new BankAccount(client1, bankInter, 2950, 156826-5)
+// const bankAccount2 = new BankAccount(client1, bankItau, 2950, 156826-5)
 
 // console.log(bankAccount1);
- console.log(bankAccount1.setCredit(100));
+// console.log(bankAccount1.setCredit(100));
 // console.log(bankAccount1.debit(40));
-console.log(bankAccount1.getAmount());
-console.log(bankAccount1.debit(100));
+// console.log(bankAccount1.getAmount());
+// console.log(bankAccount1.debit(100));
 
-console.log(bankAccount1.closeAccount());
+// console.log(bankAccount1.closeAccount());
+
+// console.log(bankAccount1.cashWithdrawal(102));
+// console.log(bankAccount1.cashWithdrawal(26));
+// console.log(bankAccount1.cashWithdrawal(15));
+
+// console.log(bankAccount2.transferTo(bankAccount1, 150)) //esta com erro: let sumFee = amount * this.bank.fee;
+
 
 
 
