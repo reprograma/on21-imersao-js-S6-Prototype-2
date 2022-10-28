14cbc4bea897ed8557ef4e4cab0e1b4900bd7033
Bárbara Pereira <barbara.pereira1990@gmail.com>
2022-10-27 22:19:02 -0300

    exercicio para casa

---
+++ exercicios/para-casa/entregas/barbara-pereira/Person.js
@@ -0,0 +1,15 @@
+
+class Person{
+    nome;
+    #cpf;
+    constructor(nome, cpf){
+        this.nome = nome;
+        this.#cpf = cpf
+    }
+}
+
+const pessoa = new Person('Maria', 11122233344);
+console.log(pessoa);
+
+module.exports = Person
+
