9a468a09e2f7a7efff204ff5d51813a8b49b9d9e
Artemiza Rocha <miza-pma@hotmail.com>
2022-10-27 22:48:43 -0300

    Exerício 6

--- exercicios/para-casa/entregas/ArtemizaRocha/BankAccount.js
+++ exercicios/para-casa/entregas/ArtemizaRocha/BankAccount.js
@@ -7,23 +7,37 @@ client;
 bank;
 accountNumber;
 agencyNumber;
-anAmount = 0;
-amountMoneyWithdrawn24hour = 0 ;
-FeeToBeChargedOnEach24HourWithdrawal = 0;
+#amount = 0;
+#qtdWithdrawal = 0 ;
+#taxWithdrawal = 0;
 
-constructor(client, bank, accountNumber, agencyNumber){
+constructor(client, bank, accountNumber, agencyNumber,){
 this.client = client;
 this.bank = bank;
 this.accountNumber = accountNumber;
 this.agencyNumber = agencyNumber;
 
+}
 
+credit(amount){
+    this.#amount += amount;
+    console.log(`Saldo atual da conta: ${this.#amount}}`);
+
+}
+
+debit(amount){
+   this.#amount -=amount;
+   console.log(`Novo Saldo ${this.#amount}`);
 }
 
 
 
-
-
 }
+
+const BankAccount1 = new BankAccount('Miza', 'C6bank', '0002', '56120')
+console.log(BankAccount1);
+BankAccount1.credit(300);
+BankAccount1.debit(250)
+
 
 module.exports = BankAccount;
