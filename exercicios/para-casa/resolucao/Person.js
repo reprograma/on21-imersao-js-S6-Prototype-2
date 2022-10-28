be090b771b794e7d699d5bc32253c6b50d2ee36d
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-10-28 19:33:32 -0300

    create Person class

---
+++ exercicios/para-casa/resolucao/Person.js
@@ -0,0 +1,17 @@
+class Person {
+	name;
+	#cpf;
+	constructor(name, cpf) {
+		this.name = name;
+		this.#cpf = cpf;
+	}
+
+	get cpf() {
+		return this.#cpf;
+	}
+}
+
+// const person1 = new Person('Maria', 12345678900)
+// console.log(person1); //Person { name: 'Maria' }
+
+module.exports = { Person };
