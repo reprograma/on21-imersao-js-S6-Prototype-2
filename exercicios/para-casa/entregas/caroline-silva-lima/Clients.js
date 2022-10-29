839de0d5219e639a3f4c46eb29f53daf1af782a6
karumundo <karumundo@gmail.com>
2022-10-29 00:56:39 -0300

    Criando o método removeBank

--- exercicios/para-casa/entregas/caroline-silva-lima/Clients.js
+++ exercicios/para-casa/entregas/caroline-silva-lima/Clients.js
@@ -23,7 +23,30 @@ class Client extends Person {
       return `Banco ${bank.bankName} adicionado com sucesso / Bank ${bank.bankName} successfully added`
     }  
   };
+
+  removeBank(bank){
+    if(!(bank instanceof Bank)){
+      throw new Error (`O parâmetro deve ser do tipo Bank / The parameter must be of type Bank`)
+    }
+    if(!this.associatedBanks.find(b => b.code === bank.code)){
+      throw new Error ('O Banco indicado não está associado a esse cliente / The indicated Bank is not associated with this customer')
+    }
+    else{
+      const i = this.associatedBanks.indexOf(bank);
+      this.associatedBanks.splice(i,1);
+      const cB = Bank.createdBanks.find(b => b.code == bank.code);
+      cB.qtyClients -= 1;
+    }
+  }
 };
+
+const bra = new Bank(2, "Bradesco", 12.5);
+const clientC = new Client("Camila Ambrósio", "jajajajaja");
+console.log(clientC.addBank(bra));
+console.log(Bank.createdBanks);
+console.log(clientC.removeBank(bra));
+console.log(Bank.createdBanks);
+console.log(clientC);
 
 module.exports = Client;
 
